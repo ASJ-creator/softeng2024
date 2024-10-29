d00ff854ded736cc580000f3797431e0a0db2ca8
ASJ-creator <dkstmdwn1@jbnu.ac.kr>
2024-10-29 18:05:42 +0900

    Update gugudan_html.py

--- hw04/gugudan_html.py
+++ hw04/gugudan_html.py
@@ -1,1 +1,16 @@
+def main():
+    color = "red"
+    filename="gugudan.html"
+    dan = 5
 
+    with open(filename, "w") as f:
+        f.write("<html>\n")
+        f.write("<body>\n")
+        for i in range(9):
+            f.write(f"{dan} X {i+1} = <font color='{color}'>{dan*(i+1)}</font><br>\n")
+        f.write("</body>\n")
+        f.write("</html>\n")
+
+
+if __name__ == "__main__":
+    main()
