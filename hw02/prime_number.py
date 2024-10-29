310c16f62e5c1cea71e83ebbfb302fd26b9d3122
ASJ-creator <dkstmdwn1@jbnu.ac.kr>
2024-10-29 15:12:42 +0900

    Update prime_number.py

--- hw02/prime_number.py
+++ hw02/prime_number.py
@@ -1,13 +1,19 @@
-from lec01.prime_number import is_prime
+def is_prime(n):
+    if n < 2:
+        return False
 
+    for i in range(2, n):
+        if n % i == 0:
+            return False
+    return True
 
 def main():
-    n=50
-    primes = []
-    for i in range(2,n + 1):
-        if is_prime(i):
-            primes.append(i)
-    print(primes)
+    n = 4
 
-if __name__ == "__main__":
+    if is_prime(n):
+        print(f"{n}은 소수입니다.")
+    else:
+        print(f"{n}은 소수가 아닙니다.")
+
+if __name__ == '__main__':
     main()
