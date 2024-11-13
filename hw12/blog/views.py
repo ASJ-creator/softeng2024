0c527888d4e0913936d85ffd47cd6877bd37c742
ASJ-creator <dkstmdwn1@jbnu.ac.kr>
2024-11-14 00:08:35 +0900

    Add files via upload

---
+++ hw12/blog/views.py
@@ -0,0 +1,23 @@
+from .models import Post
+from django.views.generic import ListView, DetailView
+# Create your views here.
+
+class PostList(ListView):
+    model = Post
+    ordering = '-pk'
+
+class PostDetail(DetailView):
+    model = Post
+# def index(request):
+#     posts = Post.objects.all().order_by('-pk')
+#     return render(request, 'blog/post_list.html',
+#     {
+#         'posts': posts
+#     })
+#
+# def single_post_page(request, pk):
+#     post = Post.objects.get(pk=pk)
+#     return render(request, 'blog/post_detail.html',
+#                   {
+#                       'post': post
+#                   })
