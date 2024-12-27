03595cc9b3bed082bbbf701e4d342314d72f9f5f
ASJ-creator <dkstmdwn1@jbnu.ac.kr>
2024-12-27 20:44:03 +0900

    Delete smart farm directory

--- smart farm/video/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-from . import views
-
-app_name = 'video'
-
-urlpatterns = [
-     path('', views.video_feed, name='video'),
-     path('realtime/', views.real_time_video, name='realtime'),
-]
